7cfae6d9a83e8b703f44991fe8bd040511697e98
Matthew Bickell <matthewbickell@hotmail.com>
2023-06-18 16:55:23 +0100

    feat: add sign out button

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -2,15 +2,19 @@ import * as React from "react";
 import styles from "./home.module.scss";
 import { View } from "../../components/view/View";
 import { useAuthContext } from "../../providers/auth";
+import { Button } from "../../components/button/Button";
 
 interface IProps {}
 
 export const HomeView: React.FC<IProps> = () => {
-  const { user } = useAuthContext();
+  const { user, signOut } = useAuthContext();
   return (
     <View className={styles.home}>
       <h1>Sparkol Tech Test</h1>
       <p>Welcome {user?.name}</p>
+      <Button variant="secondary" onClick={signOut}>
+        Sign out
+      </Button>
     </View>
   );
 };
