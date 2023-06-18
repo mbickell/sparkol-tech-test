e7233dfb7b82f2239b11902725d2606b7cb6e9ef
Matthew Bickell <matthewbickell@hotmail.com>
2023-06-18 13:25:49 +0100

    feat: Basic html and styling for login form

---
+++ src/components/View/View.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import styles from "./view.module.scss";
+import classNames from "classnames";
+
+interface IProps {
+  className?: string;
+}
+
+export const View: React.FC<React.PropsWithChildren<IProps>> = ({
+  children,
+  className
+}) => {
+  return <main className={classNames(styles.view, className)}>{children}</main>;
+};
--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,8 +1,31 @@
 import * as React from "react";
 import styles from "./login.module.scss";
+import { View } from "../../components/View/View";
 
 interface IProps {}
 
 export const LoginView: React.FC<IProps> = () => {
-  return <div className={styles.login}>LoginView</div>;
+  const login = (e: React.FormEvent) => {
+    e.preventDefault();
+    return;
+  };
+
+  return (
+    <View className={styles.login}>
+      <section className={styles.container}>
+        <h1>Login</h1>
+        <form className={styles.loginForm} onSubmit={login}>
+          <label htmlFor="username">
+            Username:
+            <input type="text" id="username" />
+          </label>
+          <label htmlFor="email">
+            Email:
+            <input type="email" id="email" />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </section>
+    </View>
+  );
 };
